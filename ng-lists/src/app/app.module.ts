368eb38250577442fb8fdd5bc49f913ff0ec45a1
= <9aviator@gmail.com>
2018-01-28 20:21:20 +0530

    Added sidebar header and changed ui of movie list items

--- ng-lists/src/app/app.module.ts
+++ ng-lists/src/app/app.module.ts
@@ -4,12 +4,16 @@ import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
