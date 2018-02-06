c9553a18054663247f1dfd1260007bcc493d3a05
Avinash Naik <avinashnaikb@gmail.com>
2018-02-06 01:58:40 +0000

    Added services and interfaces for book

---
+++ ng-lists/src/app/services/book.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Book } from '../interfaces/book';
+
+@Injectable()
+export class BookService {
+
+  constructor() { }
+
+}
